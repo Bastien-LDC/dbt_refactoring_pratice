e188fc1ca1268b2b879c3bc8323c79766838ffcf
Bastien-LDC <bastien.saclay@outlook.fr>
2023-10-11 17:34:58 +0200

    Remove join in favor of window functions + some cosmetic cleanups

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -24,83 +24,73 @@ completed_payments AS (
     GROUP BY 1
 ),
 
--- Final CTE
-
--- Simple SELECT statement
-
-
 paid_orders AS (
 
     SELECT 
-        orders.id AS order_id,
-        orders.user_id	AS customer_id,
-        orders.order_date AS order_placed_at,
-        orders.status AS order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        C.first_name AS customer_first_name,
-        C.last_name AS customer_last_name
+        orders.id AS order_id
+      , orders.user_id	AS customer_id
+      , orders.order_date AS order_placed_at
+      , orders.status AS order_status
+      , completed_payments.total_amount_paid
+      , completed_payments.payment_finalized_date
+      , customers.first_name AS customer_first_name
+      , customers.last_name AS customer_last_name
     
     FROM orders 
 
-    LEFT JOIN completed_payments AS p 
-    ON orders.id = p.order_id
+    LEFT JOIN completed_payments
+    ON orders.id = completed_payments.order_id
 
-    LEFT JOIN customers AS C 
-    ON orders.user_id = C.id
+    LEFT JOIN customers 
+    ON orders.user_id = customers.id
 ),
 
 customer_orders AS (
     SELECT 
-        C.id AS customer_id
+        customers.id AS customer_id
       , MIN(order_date) AS first_order_date
       , MAX(order_date) AS most_recent_order_date
       , COUNT(orders.id) AS number_of_orders
 
-    FROM customers C 
+    FROM customers 
 
-    LEFT JOIN orders AS Orders
-    ON orders.user_id = C.id 
+    LEFT JOIN orders
+    ON orders.user_id = customers.id 
     GROUP BY 1
+),
+
+-- Final CTE
+final AS (
+
+    SELECT
+        paid_orders.*
+      , ROW_NUMBER() OVER (ORDER BY paid_orders.order_id) AS transaction_seq
+        
+      , ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY paid_orders.order_id) AS customer_sales_seq
+
+        -- New vs Returning Customer
+      , CASE 
+            WHEN customer_orders.first_order_date = paid_orders.order_placed_at
+            THEN 'new'
+            ELSE 'return' 
+        END AS nvsr
+
+        -- Customer Lifetime Value
+      , SUM(total_amount_paid) OVER (
+            PARTITION BY paid_orders.customer_id
+            ORDER BY paid_orders.order_placed_at
+        ) AS customer_lifetime_value
+        
+        -- first day of sales
+      , customer_orders.first_order_date AS fdos
+
+    FROM paid_orders 
+
+    LEFT JOIN customer_orders USING (customer_id)
+
+    ORDER BY order_id
 )
 
-SELECT
-    p.*,
-    ROW_NUMBER() OVER (ORDER BY p.order_id) AS transaction_seq,
-    
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) AS customer_sales_seq,
+-- Simple SELECT statement
 
-    CASE 
-        WHEN c.first_order_date = p.order_placed_at
-        THEN 'new'
-        ELSE 'return' 
-    END AS nvsr,
-
-    x.clv_bad AS customer_lifetime_value,
-    c.first_order_date AS fdos
-
-FROM paid_orders p
-
-LEFT JOIN 
-    customer_orders AS c 
-USING (customer_id)
-
-LEFT OUTER JOIN (
-    SELECT
-        p.order_id,
-        SUM(t2.total_amount_paid) AS clv_bad
-
-    FROM paid_orders AS p
-
-    LEFT JOIN 
-        paid_orders AS t2 
-    ON p.customer_id = t2.customer_id 
-    AND p.order_id >= t2.order_id
-
-    GROUP BY 1
-    ORDER BY p.order_id
-) x 
-
-ON x.order_id = p.order_id
-
-ORDER BY order_id
+SELECT * FROM final
