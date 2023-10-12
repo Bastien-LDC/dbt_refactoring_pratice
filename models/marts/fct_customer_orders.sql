31a0cfc693beebe5d28d126399d582b6f44369d0
Bastien-LDC <bastien.saclay@outlook.fr>
2023-10-12 10:49:32 +0200

    Move reusable component to intermediate model + UPDT docs + tests

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -5,55 +5,29 @@ customers AS (
     SELECT * FROM {{ ref('stg_customers') }}
 ),
 
-orders AS (
-    SELECT * FROM {{ ref('stg_orders') }}
-),
-
-payments AS (
-    SELECT * FROM {{ ref('stg_payments') }}
+paid_orders AS (
+    SELECT * FROM {{ ref('int_orders') }}
 ),
 
 -- Logical CTEs
-completed_payments AS (
-    SELECT 
-        order_id 
-      , MAX(created_at) AS payment_finalized_date 
-      , SUM(amount) AS total_amount_paid
-    FROM payments
-    WHERE status <> 'fail'
-    GROUP BY 1
-),
-
-paid_orders AS (
-
-    SELECT 
-        orders.order_id
-      , orders.customer_id
-      , orders.order_date AS order_placed_at
-      , orders.status AS order_status
-      , completed_payments.total_amount_paid
-      , completed_payments.payment_finalized_date
-      , customers.first_name AS customer_first_name
-      , customers.last_name AS customer_last_name
-    
-    FROM orders 
-
-    LEFT JOIN completed_payments
-    ON orders.order_id = completed_payments.order_id
-
-    LEFT JOIN customers 
-    ON orders.customer_id = customers.customer_id
-),
-
+-- ... --
 
 -- Final CTE
 final AS (
 
     SELECT
-        paid_orders.*
+        paid_orders.order_id
+      , paid_orders.customer_id
+      , paid_orders.order_placed_at
+      , paid_orders.order_status
+      , paid_orders.total_amount_paid
+      , paid_orders.payment_finalized_date
+      , customers.first_name AS customer_first_name
+      , customers.last_name AS customer_last_name
+
       , ROW_NUMBER() OVER (ORDER BY paid_orders.order_id) AS transaction_seq
         
-      , ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY paid_orders.order_id) AS customer_sales_seq
+      , ROW_NUMBER() OVER (PARTITION BY paid_orders.customer_id ORDER BY paid_orders.order_id) AS customer_sales_seq
 
         -- New vs Returning Customer
       , CASE 
@@ -79,8 +53,9 @@ final AS (
         ) AS fdos
 
     FROM paid_orders 
+    LEFT JOIN customers 
+    ON paid_orders.customer_id = customers.customer_id
 )
-
 -- Simple SELECT statement
 
 SELECT * FROM final
